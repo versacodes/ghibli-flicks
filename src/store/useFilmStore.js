9750b9d02408a8f2b1065e97fb66b329e62ad3de
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-15 21:45:41 +0800

    Added action for fetching studio ghibli api Added films state to save fetched data Created cards in HomeView.vue for rendering the films

--- src/store/useFilmStore.js
+++ src/store/useFilmStore.js
@@ -1,6 +1,15 @@
 import { defineStore } from 'pinia'
 
-export const useFilmStore = defineStore('films', () => {
-  
+export const useFilmStore = defineStore('films', {
+  state: () => ({
+    films: "",
+  }),
+  actions: {
+    async getFilms() {
+      const res = await fetch("https://ghibliapi.vercel.app/films/")
+      const data = await res.json()
 
+      this.films = data
+    }
+  }
 })
