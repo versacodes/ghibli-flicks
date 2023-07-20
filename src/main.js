8a9551c959fe82459a0819f6dda3f1aae5cb48e6
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-20 18:40:34 +0800

    Installed primevue in project Setup primevue in main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from "primevue/config";
 import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 app.mount('#app')
